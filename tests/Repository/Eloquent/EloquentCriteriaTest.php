bfe233cc4d38c48df0a476acfdec10d9488d0dc3
brazorf <brazorf@users.noreply.github.com>
2015-11-09 20:13:58 +0100

    chore [tests] updated phpunit config.

--- tests/Repository/Eloquent/EloquentCriteriaTest.php
+++ tests/Repository/Eloquent/EloquentCriteriaTest.php
@@ -2,71 +2,70 @@
 
 class EloquentCriteriaTest extends \PHPUnit_Framework_TestCase {
 
-	public function testFilter() {
-		$criteria = new \Foothing\Repository\Eloquent\EloquentCriteria();
-		$criteria->filter('name', 'Homer');
-		$criteria->filter('lastName', 'Simpson');
-		$this->assertEquals(2, count($criteria->filters));
-		$this->assertEquals('name', $criteria->filters[0]->field);
-		$this->assertEquals('Homer', $criteria->filters[0]->value);
-		$this->assertEquals('=', $criteria->filters[0]->operator);
-		$this->assertEquals('lastName', $criteria->filters[1]->field);
-		$this->assertEquals('Simpson', $criteria->filters[1]->value);
-		$this->assertEquals('=', $criteria->filters[1]->operator);
+    public function testFilter() {
+        $criteria = new \Foothing\Repository\Eloquent\EloquentCriteria();
+        $criteria->filter('name', 'Homer');
+        $criteria->filter('lastName', 'Simpson');
+        $this->assertEquals(2, count($criteria->filters));
+        $this->assertEquals('name', $criteria->filters[0]->field);
+        $this->assertEquals('Homer', $criteria->filters[0]->value);
+        $this->assertEquals('=', $criteria->filters[0]->operator);
+        $this->assertEquals('lastName', $criteria->filters[1]->field);
+        $this->assertEquals('Simpson', $criteria->filters[1]->value);
+        $this->assertEquals('=', $criteria->filters[1]->operator);
 
-		$criteria->resetFilters()->filter('foo', 'bar', '<');
-		$this->assertEquals('<', $criteria->filters[0]->operator);
-		$criteria->resetFilters()->filter('foo', 'bar', '<=');
-		$this->assertEquals('<=', $criteria->filters[0]->operator);
-		$criteria->resetFilters()->filter('foo', 'bar', '>');
-		$this->assertEquals('>', $criteria->filters[0]->operator);
-		$criteria->resetFilters()->filter('foo', 'bar', '>=');
-		$this->assertEquals('>=', $criteria->filters[0]->operator);
-		$criteria->resetFilters()->filter('foo', 'bar', '!=');
-		$this->assertEquals('!=', $criteria->filters[0]->operator);
+        $criteria->resetFilters()->filter('foo', 'bar', '<');
+        $this->assertEquals('<', $criteria->filters[0]->operator);
+        $criteria->resetFilters()->filter('foo', 'bar', '<=');
+        $this->assertEquals('<=', $criteria->filters[0]->operator);
+        $criteria->resetFilters()->filter('foo', 'bar', '>');
+        $this->assertEquals('>', $criteria->filters[0]->operator);
+        $criteria->resetFilters()->filter('foo', 'bar', '>=');
+        $this->assertEquals('>=', $criteria->filters[0]->operator);
+        $criteria->resetFilters()->filter('foo', 'bar', '!=');
+        $this->assertEquals('!=', $criteria->filters[0]->operator);
 
-		$this->setExpectedException('Exception');
-		$criteria->resetFilters()->filter('foo', 'bar', 'invalid');
-	}
+        $this->setExpectedException('Exception');
+        $criteria->resetFilters()->filter('foo', 'bar', 'invalid');
+    }
 
     public function testOrder() {
-		$criteria = new \Foothing\Repository\Eloquent\EloquentCriteria();
-		$criteria->order('foo')->sort('asc');
-		$this->assertEquals('foo', $criteria->orderBy);
-		$this->assertEquals('asc', $criteria->sortDirection);
+        $criteria = new \Foothing\Repository\Eloquent\EloquentCriteria();
+        $criteria->order('foo')->sort('asc');
+        $this->assertEquals('foo', $criteria->orderBy);
+        $this->assertEquals('asc', $criteria->sortDirection);
 
-		$criteria->order('foo')->sort('desc');
-		$this->assertEquals('foo', $criteria->orderBy);
-		$this->assertEquals('desc', $criteria->sortDirection);
+        $criteria->order('foo')->sort('desc');
+        $this->assertEquals('foo', $criteria->orderBy);
+        $this->assertEquals('desc', $criteria->sortDirection);
 
-		$criteria->order('foo')->sort('xyz');
-		$this->assertEquals('foo', $criteria->orderBy);
-		$this->assertEquals('asc', $criteria->sortDirection);
-	}
+        $criteria->order('foo')->sort('xyz');
+        $this->assertEquals('foo', $criteria->orderBy);
+        $this->assertEquals('asc', $criteria->sortDirection);
+    }
 
     public function testMake() {
-		\Mockery::mock('alias:Illuminate\Support\Facades\Log')->shouldReceive('debug');
-		$qparams = [
-			'filter' => (object)[
-				'fields' => [
-					(object)['name' => 'email', 'value' => 'test', 'operator' => '<='],
-					(object)['name' => 'country', 'value' => 'Italy', 'operator' => 'like']
-				]
-			],
-			'sort' => (object)[
-				'field' => 'foo',
-				'direction' => 'asc'
-			]
-		];
-		$qparams = json_encode( (object)$qparams );
-		$query = \Foothing\Request\Laravel\RemoteQuery::spawn($qparams);
-		$criteria = (new \Foothing\Repository\Eloquent\EloquentCriteria())->make($query);
-		$this->assertEquals(2, count($criteria->filters));
-		$this->assertEquals('email', $criteria->filters[0]->field);
-		$this->assertEquals('test', $criteria->filters[0]->value);
-		$this->assertEquals('<=', $criteria->filters[0]->operator);
-		$this->assertEquals('foo', $criteria->orderBy);
-		$this->assertEquals('asc', $criteria->sortDirection);
-	}
-
+        \Mockery::mock('alias:Illuminate\Support\Facades\Log')->shouldReceive('debug');
+        $qparams = [
+            'filter' => (object)[
+                'fields' => [
+                    (object)['name' => 'email', 'value' => 'test', 'operator' => '<='],
+                    (object)['name' => 'country', 'value' => 'Italy', 'operator' => 'like']
+                ]
+            ],
+            'sort' => (object)[
+                'field' => 'foo',
+                'direction' => 'asc'
+            ]
+        ];
+        $qparams = json_encode( (object)$qparams );
+        $query = \Foothing\Request\Laravel\RemoteQuery::spawn($qparams);
+        $criteria = (new \Foothing\Repository\Eloquent\EloquentCriteria())->make($query);
+        $this->assertEquals(2, count($criteria->filters));
+        $this->assertEquals('email', $criteria->filters[0]->field);
+        $this->assertEquals('test', $criteria->filters[0]->value);
+        $this->assertEquals('<=', $criteria->filters[0]->operator);
+        $this->assertEquals('foo', $criteria->orderBy);
+        $this->assertEquals('asc', $criteria->sortDirection);
+    }
 }
